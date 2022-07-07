2067d072c3730cd7ba246114bfbf109912b9d1fc
You <2506641977@qq.com>
2022-07-07 23:56:05 +0800

    项目初始化-调整初始目录

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    黑马头条
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  // 组件的名字，建议为每个组件添加名字
+  name: 'App'
+}
+</script>
+
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 关闭项目中eslint报错
+  lintOnSave: false
+}
